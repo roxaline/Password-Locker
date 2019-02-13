af08b5c2de1f319a353c608db4116c978e39a63d
roxaline <mariadelarosita078@gmail.com>
2019-02-13 13:10:27 +0200

    adding some input to user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+from getpass import getpass
+
+firstName=input("enter your first name:")
+lastName=input("enter your last name:")
+Password=getpass("enter your password:")
+print("Welcome " + firstName)
